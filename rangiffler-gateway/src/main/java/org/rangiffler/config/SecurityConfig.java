03515cf393dc5438e853b8214c50180fdab2df34
alx.krw <sashkir7@mail.ru>
2023-03-24 16:04:44 +0600

    Add resourceserver to gateway

--- rangiffler-gateway/src/main/java/org/rangiffler/config/SecurityConfig.java
+++ rangiffler-gateway/src/main/java/org/rangiffler/config/SecurityConfig.java
@@ -7,25 +7,27 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
-  private final CorsCustomizer corsCustomizer;
+    private final CorsCustomizer corsCustomizer;
 
-  public SecurityConfig(CorsCustomizer corsCustomizer) {
-    this.corsCustomizer = corsCustomizer;
-  }
+    public SecurityConfig(CorsCustomizer corsCustomizer) {
+        this.corsCustomizer = corsCustomizer;
+    }
 
-  @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    corsCustomizer.corsCustomizer(http);
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        corsCustomizer.corsCustomizer(http);
 
-    http.authorizeHttpRequests(
-        authorize ->
-            authorize.anyRequest().permitAll()
-    ).csrf().disable();
-    return http.build();
-  }
+        http.authorizeHttpRequests()
+                .requestMatchers("/actuator/health").permitAll()
+                .anyRequest()
+                .authenticated().and()
+                .oauth2ResourceServer()
+                .jwt();
+
+        return http.build();
+    }
 }
