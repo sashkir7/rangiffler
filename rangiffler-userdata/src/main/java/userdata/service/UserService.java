b267f0ef8e929699e3e27820292366069c693c39
alx.krw <sashkir7@mail.ru>
2023-04-04 20:06:57 +0530

    Join classes

--- rangiffler-userdata/src/main/java/userdata/service/UserService.java
+++ rangiffler-userdata/src/main/java/userdata/service/UserService.java
@@ -6,33 +6,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import userdata.data.UserEntity;
 import userdata.data.repository.UserRepository;
-import userdata.model.UserModel;
+import userdata.model.UserDto;
 
 import javax.management.InstanceAlreadyExistsException;
 
 @Component
-public class UserRegistrationService {
+public class UserService {
 
     private final UserRepository userRepository;
 
     @Autowired
-    public UserRegistrationService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
+    public @Nonnull UserDto getCurrentUser(@Nonnull String username) {
+        // ToDo Обработать исключение, когда пользователь не найден
+        UserEntity entity = userRepository.findByUsername(username);
+        return UserDto.fromEntity(entity);
+    }
+
     @RabbitListener(queues = {"q.userdata-registration"})
-    public void saveRegistrationUser(@Nonnull UserModel userModel) throws InstanceAlreadyExistsException {
+    public void saveRegistrationUser(@Nonnull String userJson) throws InstanceAlreadyExistsException {
         // ToDo Если возникает ошибка, то сообщение перекладывается в эту же самую очередь
         //  ---> Тем самым получаем бесконечный цикл и кабзду на выходе.
         //  ---> Реализовать, чтобы в случае ошибки сообщение перекладывалось в ошибочную очередь
-        if (userRepository.findByUsername(userModel.getUsername()) != null) {
-            throw new InstanceAlreadyExistsException(userModel.getUsername());
+        UserDto userDto = UserDto.fromJson(userJson);
+
+        if (userRepository.findByUsername(userDto.getUsername()) != null) {
+            throw new InstanceAlreadyExistsException(userDto.getUsername());
         }
 
         UserEntity entity = UserEntity.builder()
-                .username(userModel.getUsername())
-                .firstname(userModel.getFirstname())
-                .lastname(userModel.getLastname())
+                .username(userDto.getUsername())
+                .firstname(userDto.getFirstname())
+                .lastname(userDto.getLastname())
                 .build();
         userRepository.save(entity);
     }
