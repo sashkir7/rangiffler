ea9af01de954a4247144e61383d0f9c3473f1188
alx.krw <sashkir7@mail.ru>
2023-04-04 18:04:57 +0530

    Remove queue name from config

--- rangiffler-auth/src/main/java/auth/config/RabbitMqConfig.java
+++ rangiffler-auth/src/main/java/auth/config/RabbitMqConfig.java
@@ -2,23 +2,18 @@ package auth.config;
 
 import org.springframework.amqp.core.Queue;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMqConfig {
 
-    private final String userdataRegistrationQueueName;
-
     @Autowired
-    public RabbitMqConfig(@Value("${rabbit-mq.userdata-registration}") String userdataRegistrationQueueName) {
-        this.userdataRegistrationQueueName = userdataRegistrationQueueName;
-    }
+    public RabbitMqConfig() {}
 
     @Bean
     public Queue createUserdataRegistrationQueue() {
-        return new Queue(userdataRegistrationQueueName);
+        return new Queue("q.userdata-registration");
     }
 
 }
--- rangiffler-auth/src/main/java/auth/service/RabbitMqService.java
+++ rangiffler-auth/src/main/java/auth/service/RabbitMqService.java
@@ -4,23 +4,20 @@ import auth.model.RegistrationModel;
 import jakarta.annotation.Nonnull;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RabbitMqService {
 
     private final RabbitTemplate rabbitTemplate;
-    private final String queueName;
 
     @Autowired
-    public RabbitMqService(RabbitTemplate rabbitTemplate,
-                           @Value("${rabbit-mq.userdata-registration}") String queueName) {
+    public RabbitMqService(RabbitTemplate rabbitTemplate) {
         this.rabbitTemplate = rabbitTemplate;
-        this.queueName = queueName;
     }
 
     public @Nonnull void sendToUserdata(@Nonnull RegistrationModel model) {
+        String queueName = "q.userdata-registration";
         rabbitTemplate.convertAndSend("", queueName, model.toJson());
     }
 
