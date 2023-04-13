b0a0709c43826c5a44be966d13acaa41dcb21da0
alx.krw <sashkir7@mail.ru>
2023-04-13 22:06:15 +0600

    Handle @WithUser annotation

--- rangiffler-test/src/test/java/jupiter/annotation/WithUser.java
+++ rangiffler-test/src/test/java/jupiter/annotation/WithUser.java
@@ -1,14 +1,21 @@
 package jupiter.annotation;
 
+import jupiter.extension.WithUserExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(ElementType.ANNOTATION_TYPE)
+@Target({ElementType.ANNOTATION_TYPE, ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
+@ExtendWith(WithUserExtension.class)
 public @interface WithUser {
     String username() default "";
     String password() default "";
+    String firstname() default "";
+    String lastname() default "";
+    String avatarClasspath() default "";
     WithPhoto[] photos() default {};
 }
