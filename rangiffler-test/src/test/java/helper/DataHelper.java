7c3310ba2c749a2d47a271262b945459479d95ba
alx.krw <sashkir7@mail.ru>
2023-04-13 22:06:49 +0600

    Get base64 from image file

--- rangiffler-test/src/test/java/helper/DataHelper.java
+++ rangiffler-test/src/test/java/helper/DataHelper.java
@@ -1,6 +1,9 @@
 package helper;
 
 import com.github.javafaker.Faker;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Base64;
 
 public final class DataHelper {
 
@@ -20,6 +23,18 @@ public final class DataHelper {
 
     public static String randomLastname() {
         return FAKER.name().lastName();
+    }
+
+    public static String imageByClasspath(String imageClasspath) {
+        ClassLoader classLoader = DataHelper.class.getClassLoader();
+        try (InputStream is = classLoader.getResourceAsStream(imageClasspath)) {
+            String fileExtension = imageClasspath.substring(imageClasspath.lastIndexOf(".") + 1);
+            assert is != null;
+            byte[] base64Image = Base64.getEncoder().encode(is.readAllBytes());
+            return "data:image/" + fileExtension + ";base64," + new String(base64Image);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
