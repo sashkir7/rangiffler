ffc3ae901e5ef7d224e81c08fbc138161dbcd1ae
alx.krw <sashkir7@mail.ru>
2023-04-14 13:19:14 +0600

    Add photo extension

--- rangiffler-test/src/test/java/jupiter/annotation/WithPhoto.java
+++ rangiffler-test/src/test/java/jupiter/annotation/WithPhoto.java
@@ -1,14 +1,20 @@
 package jupiter.annotation;
 
+import jupiter.extension.WithPhotoExtension;
+import model.CountryEnum;
+import org.junit.jupiter.api.extension.ExtendWith;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(ElementType.ANNOTATION_TYPE)
+@Target({ElementType.ANNOTATION_TYPE, ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
+@ExtendWith(WithPhotoExtension.class)
 public @interface WithPhoto {
-    String img() default "123";
-    String country() default "KZ";
+    String username() default "";
     String description() default "";
+    String imageClasspath() default "img/jpeg.jpeg";
+    CountryEnum country() default CountryEnum.RUSSIA;
 }
