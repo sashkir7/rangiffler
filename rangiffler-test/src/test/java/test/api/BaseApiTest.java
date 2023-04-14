147c08d0063bf50d3a6722fe17472173905d140c
alx.krw <sashkir7@mail.ru>
2023-04-14 10:41:51 +0600

    Move api vars to base api class

--- rangiffler-test/src/test/java/test/api/BaseApiTest.java
+++ rangiffler-test/src/test/java/test/api/BaseApiTest.java
@@ -1,6 +1,12 @@
 package test.api;
 
+import api.GeoGrpcApi;
+import api.UserdataGrpcApi;
 import test.BaseTest;
 
 public abstract class BaseApiTest extends BaseTest {
+
+    protected UserdataGrpcApi userdataApi = new UserdataGrpcApi();
+    protected GeoGrpcApi geoApi = new GeoGrpcApi();
+
 }
