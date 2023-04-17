026e2a54a3763be5bd30111fc6d10a35efc9dee6
alx.krw <sashkir7@mail.ru>
2023-04-17 15:31:58 +0600

    Add browser size

--- rangiffler-test/src/test/java/test/web/BaseWebTest.java
+++ rangiffler-test/src/test/java/test/web/BaseWebTest.java
@@ -1,7 +1,9 @@
 package test.web;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import pages.LoginPage;
 import pages.LandingPage;
 import pages.MainPage;
@@ -14,6 +16,12 @@ public abstract class BaseWebTest extends BaseTest {
     protected final LoginPage loginPage = new LoginPage();
     protected final RegistrationPage registrationPage = new RegistrationPage();
     protected final MainPage mainPage = new MainPage();
+
+
+    @BeforeAll
+    static void ab() {
+        Configuration.browserSize = "1920x1080";
+    }
 
     @AfterEach
     // ToDo - убрать в extension
