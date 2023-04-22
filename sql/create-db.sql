86acaad05f8665d2a03072edc015af2662aa2d49
alx.krw <sashkir7@mail.ru>
2023-04-22 22:42:50 +0600

    Add init create db sql

---
+++ sql/create-db.sql
@@ -0,0 +1,4 @@
+create database "auth" with owner postgres;
+create database "geo" with owner postgres;
+create database "photo" with owner postgres;
+create database "userdata" with owner postgres;
